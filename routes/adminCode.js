df5b450cd6c5f9276d41df0131ddd13e00176396
Omar El7alwany <94196070+Omar-abd-Aziz@users.noreply.github.com>
2024-06-16 12:24:08 +0300

    Add files via upload

--- routes/adminCode.js
+++ routes/adminCode.js
@@ -15,7 +15,7 @@ router.post('/', async (req, res) => {
         if (adminCodeFromClient==adminCode) {
             res.status(200).json({"statue": true}); // Respond with the updated goal
         } else {
-            res.status(200).json({"statue": false,"error": `${adminCodeFromClient}+${adminCode}`}); // Respond with the updated goal
+            res.status(200).json({"statue": false,"error": `${adminCodeFromClient} not true`}); // Respond with the updated goal
         }
     } catch (err) {
         console.error('Error AdminCode:', err);
